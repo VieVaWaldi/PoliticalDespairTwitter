d733915b763e267c6390819b8bbaaa5f15b30562
Walter Ehrenberger <wehrenberger@salesforce.com>
2023-01-15 18:23:32 +0100

    Date for tweets per user works

--- analytics/walter/1_tweets_per_user.py
+++ analytics/walter/1_tweets_per_user.py
@@ -2,6 +2,7 @@
 
 from pyspark import SparkContext, SparkConf
 import csv
+import datetime
 
 confCluster = SparkConf().setAppName("TweetsPerUser")
 sc = SparkContext(conf=confCluster).getOrCreate()
@@ -44,6 +45,7 @@ def analysis_tweets_per_user():
         tweets_numbers.append(tweets)
     return users, tweets_numbers
 
+
 def analysis_tweets_per_party():
 
     text_file = sc.textFile("data_sanitized")  # /A._McEachin.csv
@@ -64,12 +66,46 @@ def analysis_tweets_per_party():
     return party, tweets_numbers
 
 
-users, tweets_numbers = analysis_tweets_per_user()
+def date_greater_than(d1, d2='2022-03-01'):
+    try:
+        d1 = datetime.datetime.strptime(d1, '%Y-%m-%d')
+        d2 = datetime.datetime.strptime(d2, '%Y-%m-%d')
+    except:
+        return False
+    return d1 > d2
+
+
+def analysis_tweets_per_party_with_date(date_after='2022-03-01'):
+
+    text_file = sc.textFile("data_sanitized")  # /A._McEachin.csv
+
+    rdd = text_file.map(
+        lambda x: [get_columns(x)[1], get_columns(x)[2]]).filter(
+        lambda party_date: date_greater_than(
+            party_date[1], date_after)).map(
+        lambda word: ((word[0], word[1]), 1)).reduceByKey(
+            lambda a, b: a + b).sortBy(
+                lambda x: x[1]).sortBy(
+                    lambda x: x[0][1]).collect()
+
+    for i in rdd:
+        ((a, b), c) = i
+        print(a, b, c)
+        # here you can export ur data
+
+
+########## Analysis without date
+
+# users, tweets_numbers = analysis_tweets_per_user()
 # party, tweets_numbers = analysis_tweets_per_party()
 
 # Write into CSV the result
-with open('data_preprocessed/tweet_per_user.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(["username", "tweet count"])
-    for i in range(len(users)):
-        writer.writerow([users[i], tweets_numbers[i]])
+# with open('data_preprocessed/tweet_per_user.csv', 'w', newline='') as f:
+#     writer = csv.writer(f)
+#     writer.writerow(["username", "tweet count"])
+#     for i in range(len(users)):
+#         writer.writerow([users[i], tweets_numbers[i]])
+
+
+########## Analysis with date
+analysis_tweets_per_party_with_date(date_after='2022-03-01')
